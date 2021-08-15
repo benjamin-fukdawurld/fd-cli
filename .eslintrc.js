9d29513d3d708cc7015d36982eb175294b556d44
benjamin <benjamin.zamour@gmail.com>
2021-08-15 21:01:52 +0200

    Add eslint and prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: ['airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'jest'],
+  rules: {
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.test.ts', '**/*.spec.ts'],
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        ts: 'never',
+        js: 'never',
+      },
+    ],
+  },
+};
