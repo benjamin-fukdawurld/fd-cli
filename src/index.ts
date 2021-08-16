2aae9447f47b98b00eb952c2bfdc30e62d46a141
Benjamin ZAMOUR <bzamour@mediarithmics.com>
2021-08-16 09:09:10 +0200

    fixup! tmp commit

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,12 @@ import logger from './logger';
 
 const argv = {
   ...yargs(hideBin(process.argv))
+    .scriptName('fd-cli')
+    .usage(
+      '$0 <command> project-name [options]',
+      'create a backend, frontend, database or fullstack project template from command line',
+    )
+    .usage('$0', 'create a backend, frontend, database or fullstack project template interactively')
     /*
 .usage(
   '$0 project <project-name> [options]',
@@ -14,13 +20,51 @@ const argv = {
   (args: yargs.Argv<{}>): void => {},
 )
 */
-    .command('back', 'generate back end only')
-    .command('front', 'generate front end only')
-    .command('database', 'generate PostgreSQL database docker image template')
+    .command('back', 'generate back end only', (yargs: yargs.Argv<any>): yargs.Argv<any> => {
+      return yargs.positional('name', {
+        type: 'string',
+        describe: 'the name of the projects',
+      });
+    })
+    .command('front', 'generate front end only', (yargs: yargs.Argv<any>): yargs.Argv<any> => {
+      return yargs.positional('name', {
+        type: 'string',
+        describe: 'the name of the projects',
+      });
+    })
+    .command(
+      'database',
+      'generate PostgreSQL database docker image template',
+      (yargs: yargs.Argv<any>): yargs.Argv<any> => {
+        return yargs.positional('name', {
+          type: 'string',
+          describe: 'the name of the projects',
+        });
+      },
+    )
     .command(
       'project',
       'generate project including, back end, front end and a PostrgreSQL database image if -pg option is set',
+      (yargs: yargs.Argv<any>): yargs.Argv<any> => {
+        return yargs.positional('name', {
+          type: 'string',
+          describe: 'the name of the projects',
+        });
+      },
     )
+    .option('mongo', { alias: 'mdb', description: 'add MongoDB utils to backend', type: 'boolean' })
+    .option('postgres', {
+      alias: 'pg',
+      description: 'add postgres utils to back end and create de prosgres DockerImage template',
+      type: 'boolean',
+    })
+    .option('graphql', { alias: 'gql', description: 'add gql utils to backend', type: 'boolean' })
+    .option('knex', { description: 'add knex to backend dependencies', type: 'boolean' })
+    .option('typeorm', { alias: 'torm', description: 'add typeorm to backend dependencies' })
+    .option('material', { alias: 'mui', description: 'add material ui to frontend' })
+    .option('tailwind', { alias: 'tw', description: 'add tailwind to frontend' })
+    .option('sass', { description: 'add sass to frontend', type: 'boolean' })
+
     .help()
     .alias('h', 'help').argv,
 };
